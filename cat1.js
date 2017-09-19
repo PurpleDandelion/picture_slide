e39c2e8fa9f2c19c20fc1914ad867ab4eaa1c9be
dandelion <xuchangxia0712@163.com>
2017-09-19 18:18:37 +0800

    update mvo

---
+++ cat1.js
@@ -0,0 +1,38 @@
+/* ============ model =========== */
+
+var model = {
+    currentCat: null,
+	cats: [
+        {
+            clickCount : 0,
+            name : '谁在看本宝宝',
+            imgSrc : 'img/watch.jpg',
+        },
+        {
+            clickCount : 0,
+            name : 'two',
+            imgSrc : 'img/2.jpg',
+        },
+        {
+            clickCount : 0,
+            name : '睡觉啦',
+            imgSrc : 'sleep.jpg',
+        },
+        {
+            clickCount : 0,
+            name : '你跳我就跳',
+            imgSrc : 'img/jump.jpg',
+        },
+        {
+            clickCount : 0,
+            name : '本宝宝生气了',
+            imgSrc : 'img/angry.jpg',
+        }
+    ]
+};
+
+/* =========== octopus ===========*/
+var octopus = {};
+
+/* ========== view ==============*/
+var view = {};
